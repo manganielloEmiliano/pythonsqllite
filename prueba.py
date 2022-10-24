5c1a12405e9074ddf74c0cd63067e2c9edd9b874
emi <lucasemilianomanganiello@gmail.com>
2022-10-24 13:53:35 -0300

    elimine la opcion de seguir agregando dentro de la opcion 1 porque daba un error

---
+++ prueba.py
@@ -0,0 +1,130 @@
+import sqlite3
+
+
+def conection_db():
+    bd = sqlite3.connect("Empleados.db")
+    return bd
+
+
+def crear_tabla(cursor):
+
+    cursor.execute('''CREATE TABLE  IF NOT  EXISTS empleados (
+	"id"	INTEGER NOT NULL,
+	"numero_legajo"	INTEGER NOT NULL UNIQUE,
+    "dni"	INTEGER NOT NULL UNIQUE,
+	"nombre_empleado"	TEXT NOT NULL,
+	"apellido_empleado"	TEXT NOT NULL,
+	"area"	TEXT NOT NULL,
+	PRIMARY KEY("id"AUTOINCREMENT));'''
+                   )
+
+
+def insertar_nuevo_registro(bd, cursor, valores):
+    cursor.execute("INSERT INTO empleados VALUES(NULL,?,?,?,?,?)", valores)
+
+    bd.commit()
+    print("registro insertado")
+    bd.close()
+    
+
+
+def seleccionar_unregistro(cursor, dni):
+    cursor.execute('SELECT * FROM empleados WHERE dni=?', [dni])
+    print(cursor.fetchone())
+
+
+def seleccionar_todos_registros(cursor):
+    cursor.execute('SELECT * FROM Empleados',)
+    print(cursor.fetchall())
+
+
+def modificar_registros(bd, cursor, numero_legajo, area):
+    sentencia = "UPDATE empleados SET area = ?  WHERE numero_legajo = ?;"
+    cursor.execute(sentencia, [area, numero_legajo])
+    bd.commit()
+    print("el area de trabajo del empleado con el legajo {} fue actualizada a {}".format(
+        numero_legajo, area))
+    bd.close()
+
+
+def eliminar_registros(bd, cursor, numero_legajo):
+
+    sentencia = "DELETE FROM empleados WHERE numero_legajo = ?;"
+    cursor.execute(sentencia, [numero_legajo])
+    bd.commit()
+    print("Datos eliminados")
+    bd.close()
+
+    bd = conection_db
+    cursor = bd.cursor()
+    crear_tabla(cursor)
+
+
+while True:
+   
+    
+
+    print("ingrese la opcion que desea ejecutar ")
+    print("Opción 1: Insertar un registro de empleado")
+    print("Opción 2: Seleccionar un registro de empleado a partir de su número de DNI")
+    print("Opción 3: Seleccionar todos los empleados o los registros de la tabla")
+    print("Opción 4: Modificar el área de un empleado en función de su número de legajo")
+    print("Opción 5: Eliminar un empleado a partir de su número de legajo")
+    print("Opción 6: Finalizar")
+    opcion = input("ingrese la opcion: ")
+
+    if opcion == "1":
+
+        try:
+
+            valores = []
+            valor1 = int(input("ingrese el numero de legajo "))
+            valores.append(valor1)
+            valor2 = int(input("ingrese el dni: "))
+            valores.append(valor2)
+            valor3 = input("ingrese el nombre: ")
+            valores.append(valor3)
+            valor4 = input("ingrese el apellido: ")
+            valores.append(valor4)
+            valor5 = input("ingrese el area en la que trabaja: ")
+            valores.append(valor5)
+            bd.conection_db()
+            insertar_nuevo_registro(bd, cursor, valores)
+           
+        except ValueError:
+            print("ingrese los datos del tipo correcto")
+    elif opcion == "2":
+        bd.conection_db()
+        while True:
+            try:
+                seleccionar_unregistro(cursor, input("ingrese un dni: "))
+                break
+            except ValueError:
+                print("el dni debe ser numerico")
+    elif opcion == "3":
+        bd.conection_db()
+        seleccionar_todos_registros(cursor)
+    elif opcion == "4":
+        bd.conection_db()
+        while True:
+            try:
+                numero_legajo = int(input("ingrese el numero de legajo: "))
+                area = input("ingrese la nueva area: ")
+                modificar_registros(bd, cursor, numero_legajo, area)
+               
+                break
+            except ValueError:
+                print("el legajo debe ser numerico")
+    elif opcion == "5":
+        bd.conection_db()
+        while True:
+            try:
+                numero_legajo = int(input("ingrese el numero de legajo: "))
+                break
+            except ValueError:
+                print("ingrese un valor entero")
+        eliminar_registros(bd, cursor, numero_legajo)
+    else:
+        bd.commit()
+        bd.close()
+        break
--- sistemaempledos.py
+++ sistemaempledos.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 
@@ -6,8 +5,9 @@ def conection_db():
     bd = sqlite3.connect("Empleados.db")
     return bd
 
+
 def crear_tabla(cursor):
-    
+
     cursor.execute('''CREATE TABLE  IF NOT  EXISTS empleados (
 	"id"	INTEGER NOT NULL,
 	"numero_legajo"	INTEGER NOT NULL UNIQUE,
@@ -15,51 +15,55 @@ def crear_tabla(cursor):
 	"nombre_empleado"	TEXT NOT NULL,
 	"apellido_empleado"	TEXT NOT NULL,
 	"area"	TEXT NOT NULL,
-	PRIMARY KEY("id"AUTOINCREMENT));'''
-    )
+	PRIMARY KEY("id"AUTOINCREMENT));''')
 
-def insertar_nuevo_registro(cursor,valores):
-    conection_db()
-    cursor.execute("INSERT INTO empleados VALUES(NULL,?,?,?,?,?)",valores)
+
+def insertar_nuevo_registro(bd, cursor, valores):
+
+    cursor.execute("INSERT INTO empleados VALUES(NULL,?,?,?,?,?)", valores)
     bd.commit()
     bd.close()
 
-def seleccionar_unregistro(cursor,dni):
-   
+
+def seleccionar_unregistro(cursor, dni):
+
     cursor.execute('SELECT * FROM empleados WHERE dni=?', [dni])
     print(cursor.fetchone())
-  
+
 
 def seleccionar_todos_registros(cursor):
-    
+    db = conection_db()
+
     cursor.execute('SELECT * FROM empleados',)
     print(cursor.fetchall())
-   
+    bd.commit()
+    bd.close()
 
-def modificar_registros(cursor, numero_legajo,area):
-    conection_db()
+
+def modificar_registros(bd, cursor, numero_legajo, area):
+
     sentencia = "UPDATE empleados SET area = ?  WHERE numero_legajo = ?;"
     cursor.execute(sentencia, [area, numero_legajo])
     bd.commit()
     bd.close()
-    print("el area de trabajo del empleado con el legajo {} fue actualizada a{}".format(numero_legajo,area))
+    print("el area de trabajo del empleado con el legajo {} fue actualizada a{}".format(
+        numero_legajo, area))
 
 
-def eliminar_registros(cursor, numero_legajo):
-    conection_db()
+def eliminar_registros(bd, cursor, numero_legajo):
+
     sentencia = "DELETE FROM empleados WHERE numero_legajo = ?;"
     cursor.execute(sentencia, [numero_legajo])
     bd.commit()
     bd.close()
     print("Datos eliminados")
 
-bd=conection_db()
-cursor = bd.cursor()
-
-crear_tabla(cursor)
 
 while True:
-    
+    bd = conection_db()
+    cursor = bd.cursor()
+    crear_tabla(cursor)
+
     print("ingrese la opcion que desea ejecutar ")
     print("Opción 1: Insertar un registro de empleado")
     print("Opción 2: Seleccionar un registro de empleado a partir de su número de DNI")
@@ -67,70 +71,70 @@ while True:
     print("Opción 4: Modificar el área de un empleado en función de su número de legajo")
     print("Opción 5: Eliminar un empleado a partir de su número de legajo")
     print("Opción 6: Finalizar")
-    opcion=input("ingrese la opcion: ")
-    if opcion =="1":
+    opcion = input("ingrese la opcion: ")
+    if opcion == "1":
         while True:
+
             try:
-                
-                valores=[]
-                
-                valor1=int(input("ingrese el numero de legajo "))
+
+                valores = []
+
+                valor1 = int(input("ingrese el numero de legajo "))
                 valores.append(valor1)
-                valor2=int(input("ingrese el dni: "))
+                valor2 = int(input("ingrese el dni: "))
                 valores.append(valor2)
-                valor3=input("ingrese el nombre: ")
+                valor3 = input("ingrese el nombre: ")
                 valores.append(valor3)
-                valor4=input("ingrese el apellido: ")
+                valor4 = input("ingrese el apellido: ")
                 valores.append(valor4)
-                valor5=input("ingrese el area en la que trabaja: ")
+                valor5 = input("ingrese el area en la que trabaja: ")
                 valores.append(valor5)
-                bd=conection_db()
-                insertar_nuevo_registro(cursor,valores)  
-                salir=input("¿desea continuar ingresando registros? s/n: ")
-                if salir=="n":
-                    break
+
+                insertar_nuevo_registro(bd, cursor, valores)
+                break
             except ValueError:
                 print("ingrese los datos del tipo correcto")
-           
-    elif opcion =="2":
+
+    elif opcion == "2":
         try:
-            bd=conection_db()
-            while TRUE:
+
+            while True:
                 try:
-                    seleccionar_unregistro(cursor,input("ingrese un dni: "))
+                    seleccionar_unregistro(cursor, input("ingrese un dni: "))
                     break
                 except ValueError:
                     print("el dni debe ser numerico")
         except ValueError:
             print("ingrese un dni correcto")
-    
-    elif opcion =="3":
-        conection_db()
+
+    elif opcion == "3":
+
         seleccionar_todos_registros(cursor)
-        
+
     elif opcion == "4":
-        bd=conection_db()
-        while True:
-            try:
-                numero_legajo =int(input("ingrese el numero de legajo: "))
-                area = input("ingrese la nueva area: ")
-                modificar_registros(cursor,numero_legajo,area)
-                bd=conection_db()
-                break
-            except ValueError:
-                print("el legajo debe ser numerico")
-    elif opcion == "5":
-        bd=conection_db()
+
         while True:
             try:
                 numero_legajo = int(input("ingrese el numero de legajo: "))
+                area = input("ingrese la nueva area: ")
+                modificar_registros(bd, cursor, numero_legajo, area)
+
                 break
             except ValueError:
-                print("ingrese un valor entero")    
-        eliminar_registros(cursor,numero_legajo)
-        bd=conection_db()
-        
-    else :
-        bd.close()
+                print("el legajo debe ser numerico")
+
+    elif opcion == "5":
+
+        while True:
+            try:
+                numero_legajo = int(input("ingrese el numero de legajo: "))
+
+                break
+
+            except ValueError:
+                print("ingrese un valor entero")
+
+        eliminar_registros(bd, cursor, numero_legajo)
+
+    else:
         break
-    
